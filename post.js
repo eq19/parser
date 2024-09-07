01d0e12080a4641a7250c69befb0bf25180406a1
eQuantum <8466209+eq19@users.noreply.github.com>
2024-09-07 13:16:43 +0700

    Update post.js

--- post.js
+++ post.js
@@ -27,6 +27,11 @@ pgclient.query('SELECT * FROM student', (err, res) => {
     console.log(err, res.rows) // Print the data in student table
 });
 
+pgclient.query('SELECT * FROM car_sales WHERE customer_id=1000', (err, res) => {
+    if (err) throw err
+    console.log(err, res.rows) // Print the data in student table
+});
+
 pgclient.query('SELECT table_schema,table_name FROM information_schema.tables', (err, res) => {
     if (err) throw err
     console.log(err, res.rows) // Print the data in student table
